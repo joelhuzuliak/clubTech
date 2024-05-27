e496f74f7d690378d66fe2c2206c8649479273f5
Joel Huzuliak <joelhuzuliak10@hotmail.com>
2024-05-27 16:10:36 -0300

    first commit

---
+++ js/productos.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    var swiper = new Swiper('.mySwiper', {
+        // Configuración de Swiper
+        slidesPerView: 3,
+        spaceBetween: 30,
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
+});
